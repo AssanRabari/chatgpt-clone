77daccfc8cbb1a62ffb5ec12f211e04065218891
Assan Rabari <assandesai@gmail.com>
2025-01-13 09:52:43 +0530

    Homepage left section design

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -4,8 +4,19 @@ import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>
-      <Link to="/dashboard">Dashboard</Link>
+    <div className="home-page">
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>AI Chat</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          minus ratione harum eum libero a vitae ipsa totam dolor
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+        {/* <button>Get Started</button> */}
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
