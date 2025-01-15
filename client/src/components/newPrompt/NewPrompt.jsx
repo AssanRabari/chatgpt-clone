1bc10c9a1082560d5a0ab652e195425b998de2be
Assan Rabari <assandesai@gmail.com>
2025-01-15 23:22:23 +0530

    gpt-clone: Added upload image functionality imagekit using node server

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,17 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./NewPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
 const NewPrompt = () => {
+  const [image, setImage] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
   const endRef = useRef(null);
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
   return (
     <>
+      {image.isLoading && <div>Loading...</div>}
+      {image.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={image.dbData?.filePath}
+          width="380" 
+          transformation={[{width:380}]}
+        />
+      )}
       <div className="end-chat" ref={endRef}></div>
       <div className="new-form">
-        <label htmlFor="file">
-          <img src="/attachment.png" alt="" />
-        </label>
+        <Upload setImage={setImage} />
         <input id="file" type="file" multiple={false} hidden />
         <input type="text" placeholder="Ask anything..." />
         <button>
