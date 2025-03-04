8ce17f36e32c46ddb689e59d896ce6ad3884cd24
Assan Rabari <assandesai@gmail.com>
2025-03-04 10:21:53 +0530

    Implemented gemini api

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -2,16 +2,37 @@ import React, { useEffect, useRef, useState } from "react";
 import "./NewPrompt.css";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   const [image, setImage] = useState({
     isLoading: false,
     error: "",
     dbData: {},
   });
   const endRef = useRef(null);
+
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
+
+  const add = async (text) => {
+    setQuestion(text)
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text());
+    console.log(result.response.text());
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+  };
+
   return (
     <>
       {image.isLoading && <div>Loading...</div>}
@@ -19,19 +40,23 @@ const NewPrompt = () => {
         <IKImage
           urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
           path={image.dbData?.filePath}
-          width="380" 
-          transformation={[{width:380}]}
+          width="380"
+          transformation={[{ width: 380 }]}
         />
       )}
+
+      {question && <div className="message user">{question}</div>}
+      {answer && <div className="message">{answer}</div>}
+
       <div className="end-chat" ref={endRef}></div>
-      <div className="new-form">
+      <form className="new-form" onSubmit={handleSubmit}>
         <Upload setImage={setImage} />
         <input id="file" type="file" multiple={false} hidden />
-        <input type="text" placeholder="Ask anything..." />
+        <input type="text" name="text" placeholder="Ask anything..." />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
-      </div>
+      </form>
     </>
   );
 };
