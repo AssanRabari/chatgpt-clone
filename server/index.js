e536c2097c2fc96dbc060497597abbd1168d4d9f
Assan Rabari <assandesai@gmail.com>
2025-03-06 10:12:31 +0530

    Setup mongodb connection, created basic post route to save data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ImageKit from "imagekit";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,16 @@ app.use(
     origin: process.env.CLIENT_URL,
   })
 );
+
+app.use(express.json());
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Error connecting database", error);
+  }
+};
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
   publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
@@ -19,6 +30,13 @@ app.get("/api/upload", (req, res) => {
   const result = imagekit.getAuthenticationParameters();
   res.send(result);
 });
+
+app.post("/api/chats", (req, res) => {
+  const { text } = req.body;
+  console.log("text----------", text);
+});
+
 app.listen(port, () => {
+  connect();
   console.log(`Server is runing on port ${port}`);
 });
